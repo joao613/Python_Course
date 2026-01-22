bcb6f52057467c5f5779a964e2caf917933e3595
Joao <jpmf2000@gmail.com>
2026-01-22 14:30:25 +0000

    add madlibs exercise

---
+++ Exercises/Ex3_madlibs.py
@@ -0,0 +1,14 @@
+# Madlibs Game
+# Word game where you fill blanks with random words to create a story
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb1 = input("Ener a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective: ")
+
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
